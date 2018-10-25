91366359a04ef66fd05e5185216f4cf92f925a48
WicopeeDot <wicobotdiscord@gmail.com>
2018-10-26 01:08:21 +0300

    make it functional

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
-const mongoose = require('mongoose')
+const mongodb = require('mongodb')
 const config = require('./config.json')
 const bodyParser = require('body-parser')
 const request = require('request')
+const util = require('./util')
 
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http)
 
+let db;
+
 const url = `mongodb://${config.mdbuser}:${config.mdbpass}@${config.mdbhost}:${config.mdbport}/?authSource=admin&authMechanism=${config.mdbauthmetd}`;
-mongoose.connect(url, err => { if(err) console.log(err) })
+mongodb.connect(url, { useNewUrlParser: true }, (err,dat) => { if(err) throw err; db = dat.db('penexpress'); })
 
 app.use(express.static('static'));
 app.use(express.static('node_modules'));
@@ -22,6 +25,69 @@ app.get('/', function(req, res) {
 	res.render('index', { captcha: config.recaptchasite })
 })
 
+app.get('/pedit/:id', async (req, res) => {
+	try {
+		let id = new mongodb.ObjectId(req.params.id)
+		let data = await db.collection('posts').find(id).toArray()
+		data = data[0]
+
+		if(!data)
+			res.render('404', { id: req.params.id })
+		else
+			res.render('edit', { text: data.content, id: req.params.id, captcha: config.recaptchasite })
+	} catch(err) {
+		res.render('404', { id: req.params.id })
+	}
+})
+
+app.get('/p/:id', async (req, res) => {
+	try {
+		let id = new mongodb.ObjectId(req.params.id)
+		let data = await db.collection('posts').find(id).toArray()
+		data = data[0]
+
+		if(!data)
+			res.render('404', { id: req.params.id })
+		else
+			res.render('page', { text: data.content, id: req.params.id  })
+	} catch(err) {
+		res.render('404', { id: req.params.id })
+	}
+})
+
+app.post('/submedit', async (req, res) => {
+	let options = {
+		secret: config.recaptchasecret,
+		response: req.body["g-recaptcha-response"]
+	}
+
+	request.post('https://www.google.com/recaptcha/api/siteverify', {form: options}, async function(_,__,resp) {
+		resp = JSON.parse(resp)
+		if(!resp.success) 
+			return res.status(400).send({success:false,msg:'captcha not correct'})
+
+		let id = new mongodb.ObjectId(req.body.__id)
+
+		let yes = await db.collection('posts').find(id).toArray()
+		yes = yes[0]
+
+		if(!yes)
+			return res.send({success:false,msg:'could not find document'}).status(400)
+
+		util.comparePassword(req.body.pswd, yes.pswd, async function(err, resp1) {
+			console.log(err)
+			if(err)
+				return res.status(500);
+
+			if(!resp1)
+				return res.status(400).send({success:false,msg:'incorrect password'})
+
+			let resp2 = await db.collection('posts').updateOne({_id: id}, {$set: {content: req.body.new_content}})
+			res.redirect('/p/' + req.body.__id)
+		})
+	});
+});
+
 app.post('/subm', async (req, res) => {
 	let options = {
 		secret: config.recaptchasecret,
@@ -33,7 +99,22 @@ app.post('/subm', async (req, res) => {
 		if(!resp.success) 
 			return res.status(400).send({success:false,msg:'captcha not correct'})
 
-		res.send('egg')
+		if(!db)
+			return res.status(400).send({success:false,msg:'database not available'})
+
+		util.cryptPassword(req.body.pswd, async function(err, hash) {
+			console.log(err)
+			if(err)
+				return res.status(500); 
+
+			let obj = {
+				pswd: hash,
+				content: req.body.text
+			}
+
+			let resp1 = await db.collection('posts').insertOne(obj)
+			res.redirect('/p/' + resp1.insertedId)
+		})
 	});
 })
 
--- static/index.js
+++ static/index.js
@@ -1,8 +1,14 @@
-var typed = new Typed('#typed', {
-	strings: ["Write anything down."],
-	typeSpeed: 25,
-	onComplete: function() {
-		$('#appearaftertype').css('opacity', '1')
-		$('#appearaftertype').addClass('appear')
-	}
-});
+$(function() {
+	try {
+		var typed = new Typed('#typed-index', {
+			strings: ["Write anything down."],
+			typeSpeed: 25,
+			onComplete: function() {
+				$('#appearaftertype').css('opacity', '1')
+				$('#appearaftertype').addClass('appear')
+			}
+		});
+	} catch(err) {}
+
+	$('.pageContent').html(markdown.toHTML($('.pageContent').text()))
+})
