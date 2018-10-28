e582552395a5a358b949c35eebe33d5d0ee156d2
W1C0P33 <wicobotdiscord@gmail.com>
2018-10-28 17:53:41 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,9 +1,26 @@
-try {
-  require('./index.js')
-  setTimeout(function() {
-    process.exit(0);
-  }, 10000)
-} catch (err) {
-  console.error(err);
-  process.exit(1);
-}
+(async function() {
+  try {
+    let fs = require('fs')
+    
+    let opts = {
+      mdbhost: "127.0.0.1",
+      mdbport: "27017",
+      mdbuser: "admin",
+      mdbpass: "admin",
+      mdbauthmetd: "DEFAULT",
+      recaptchasite: "SITEKEY",
+      recaptchasecret: "SECRETKEY"
+    }
+    
+    await fs.writeFileSync('config.json', JSON.stringify(opts))
+    
+    require('./index.js')
+
+    setTimeout(function() {
+      process.exit(0);
+    }, 10000)
+  } catch (err) {
+    console.error(err);
+    process.exit(1);
+  }
+})
